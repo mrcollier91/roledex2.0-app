4c04f154f911ef91e044c7da3926ffe35ab1c26d
mrcollier91 <colliermatt91@gmail.com>
2022-10-13 11:01:43 -0500

    fetch for db.json contact data renedered in list for contact set state for contacts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ContactList from './ContactList';
+
 
 function App() {
   return (
     <div className="App">
+      <ContactList/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/contactList.js
+++ src/contactList.js
@@ -1,9 +1,35 @@
+import { useState, useEffect } from "react"
 
 
 function ContactList() {
+
+    const [contacts, setContacts] = useState([])
+    
+    function getContacts() {
+        return fetch("http://localhost:3004/contacts")
+        .then(response => response.json())
+        .then(data => setContacts(data))
+    }
+
+    useEffect(()=> {
+        getContacts()
+    },[])
+
+
     return(
         <div>
-
+            {
+            contacts.map((contact) =>{
+            return(
+            <div key={contact.id}>
+            <p>{contact.name}</p>
+            <p>{contact.phone}</p>
+            <p>{contact.email}</p>
+            <p>{contact.address}</p>
+            </div>
+            )
+            })
+            }
         </div>
     )
 }
