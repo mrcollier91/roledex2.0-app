394fa62365a70f2157b8d3f3c8ed6983000c34e9
mrcollier91 <colliermatt91@gmail.com>
2022-10-13 11:55:51 -0500

    individual contact fetch add display if endpoint matches

--- src/IndividualContact.js
+++ src/IndividualContact.js
@@ -1,9 +1,38 @@
+import {useEffect, useState} from "react"
+import {useParams} from "react-router-dom"
+
 
 
 function IndividualContact() {
+    const [contact, setContact] = useState(null)
+    const params = useParams()
+
+    function getContact() {
+        return fetch(`http://localhost:3004/contacts/${params.id}`)
+          .then(response => response.json())
+          .then(data => setContact(data))
+      }
+    
+      useEffect(()=> {
+        getContact()
+    },[])
+
     return(
         <div>
-
+            {contact ? 
+                
+                (
+                     <div key={contact.id}>
+                     <p>{contact.name}</p>
+                     <p>{contact.address}</p>
+                     <p>{contact.phone}</p>
+                     <p>{contact.email}</p>
+                     </div>
+                )
+                : <div>Contact doesn't exist</div>
+             }
+        
+         
         </div>
     )
 }
