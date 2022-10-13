d2bdd61cff8c258d694eec1ac48d0a33d4b8471f
mrcollier91 <colliermatt91@gmail.com>
2022-10-13 11:58:18 -0500

    navbar added to pages

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,17 @@
-
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
     return(
         <div>
-
+            <NavLink to="/">
+             Home
+             </NavLink>
+            <NavLink to="/contacts">
+            Contacts
+            </NavLink>
+            <NavLink to="/newcontactform">
+            New contact Form
+            </NavLink>
         </div>
     )
 }
