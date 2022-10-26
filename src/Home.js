ee22802a443b2e4a76861ac20f5d69c6913accd6
mrcollier91 <colliermatt91@gmail.com>
2022-10-26 11:09:42 -0500

    add instructions to home pagehow to add new contacts

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,9 @@ function Home() {
     return(
         <div>
             <h1>Welcome to Roledex app 2.0</h1>
+            <p>instructions:</p>
+            <br></br>
+            <p>use new contact form page to add new contacts</p>
          
         </div>
     )
