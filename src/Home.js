5c18650ecd5ad2bee0bb69c0eca6c303fd47d273
mrcollier91 <colliermatt91@gmail.com>
2022-10-26 11:11:18 -0500

    add instruction on navigating to indivitual contact

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,12 @@ function Home() {
             <h1>Welcome to Roledex app 2.0</h1>
             <p>instructions:</p>
             <br></br>
-            <p>use new contact form page to add new contacts</p>
+            <p>-use new contact form page to add new contacts name and email are required</p>
+            <br></br>
+            <p>-from contacts you can sort contacts in Alphabetical order using the sort button</p>
+            <br></br>
+            <p>-you can click on a contact from contacts and it will take you to their IndividualContact card</p>
+
          
         </div>
     )
